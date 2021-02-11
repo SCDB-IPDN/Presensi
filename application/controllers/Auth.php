f0c262cd0d03e5d68916a602ab49ed01373d11af
Maulyanda <maulyanda94.kcb@gmail.com>
2021-02-11 13:25:22 +0700

    Update auth

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -12,11 +12,27 @@ class Auth extends CI_Controller
     public function index()
     {
         //CEK IP PUBLIC
-        $externalContent = file_get_contents('http://checkip.dyndns.com/');
-        preg_match('/Current IP Address: \[?([:.0-9a-fA-F]+)\]?/', $externalContent, $m);
-        $externalIp = $m[1];
+        // $externalContent = file_get_contents('http://checkip.dyndns.com/');
+        // preg_match('/Current IP Address: \[?([:.0-9a-fA-F]+)\]?/', $externalContent, $m);
+        // $externalIp = $m[1];
 
-        $cek_ip = $this->db->query("SELECT * FROM tbl_ip WHERE ip = '$externalIp'")->result();
+        $ipaddress = '';
+        if (getenv('HTTP_CLIENT_IP'))
+            $ipaddress = getenv('HTTP_CLIENT_IP');
+        else if(getenv('HTTP_X_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+        else if(getenv('HTTP_X_FORWARDED'))
+            $ipaddress = getenv('HTTP_X_FORWARDED');
+        else if(getenv('HTTP_FORWARDED_FOR'))
+            $ipaddress = getenv('HTTP_FORWARDED_FOR');
+        else if(getenv('HTTP_FORWARDED'))
+        $ipaddress = getenv('HTTP_FORWARDED');
+        else if(getenv('REMOTE_ADDR'))
+            $ipaddress = getenv('REMOTE_ADDR');
+        else
+            $ipaddress = 'IP tidak dikenali';
+
+        $cek_ip = $this->db->query("SELECT * FROM tbl_ip WHERE ip = '$ipaddress'")->result();
 
         //CEK MOBILE
         $ua = strtolower($_SERVER['HTTP_USER_AGENT']);
